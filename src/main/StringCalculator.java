3f758f193b32659589b91aef8e9896467871db72
Juan Manuel Pérez Toro <juanma.perez@autentia.com>
2019-12-16 09:35:22 +0100

    Add return separator

--- src/main/StringCalculator.java
+++ src/main/StringCalculator.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 public class StringCalculator {
     public int add(String s) {
         if(s.length() == 0) return 0;
-        Optional<Integer> result = Arrays.stream(s.split(",")).map(Integer::parseInt).reduce(Integer::sum);
+        Optional<Integer> result = Arrays.stream(s.split("[,\\n]")).map(Integer::parseInt).reduce(Integer::sum);
         return result.orElse(0);
     }
 }
--- src/test/StringCalculatorTest.java
+++ src/test/StringCalculatorTest.java
@@ -32,8 +32,26 @@ public class StringCalculatorTest {
     }
 
     @Test
-    public void given_multiple_numbers_should_return_their_sum () {
+    public void given_multiple_numbers_should_return_their_sum() {
         assertEquals(7, stringCalculator.add("1,1,1,1,1,1,1"));
         assertEquals(10, stringCalculator.add("2,3,5"));
+    }
+
+    @Test
+    public void given_two_numbers_with_return_separator_should_return_their_sum() {
+        assertEquals(2, stringCalculator.add("1\n1"));
+        assertEquals(3, stringCalculator.add("1\n2"));
+        assertEquals(12, stringCalculator.add("5\n7"));
+    }
+
+    @Test
+    public void given_multiple_numbers_with_return_and_comma_separators_should_return_their_sum() {
+        assertEquals(3, stringCalculator.add("1,1\n1"));
+        assertEquals(5, stringCalculator.add("1,3\n1"));
+    }
+
+    @Test
+    public void given_one_number_ending_with_return_separator_only_return_the_number() {
+        assertEquals(1, stringCalculator.add("1\n"));
     }
 }
